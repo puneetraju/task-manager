d890a796b71dbee17997515960ef1f89219e865b
Puneet Raju <puneet02ujar@gmail.com>
2025-02-16 05:00:34 +0530

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,15 @@
 import "../styles/globals.css";
 
+export const metadata = {
+  title: {
+    default: 'Task Manager(Yardstick)', // Default title
+  },
+  description: 'Manage your tasks efficiently with Task Manager.',
+  icons: {
+    icon: '/favicon.ico', // Path to your favicon
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
